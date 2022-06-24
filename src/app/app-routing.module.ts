b36173711b8b43e425c9b010f95811976eea0aa5
Rutu Dave <rutu.dave47@gmail.com>
2022-06-24 11:56:37 +0530

    Add routing for list and detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostListComponent } from './post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: PostListComponent
+  },
+  {
+    path: 'post-detail',
+    component: PostDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
